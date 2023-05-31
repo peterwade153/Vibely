ec8e45fbf235a6fe295aa6f37437737f3ed82964
peterwade153 <peterwade153@gmail.com>
2023-05-31 15:11:49 +0300

    write to file urilize chunsize

--- wrangler.py
+++ wrangler.py
@@ -18,15 +18,5 @@ def music_total_day_plays(file_path, chunks):
     df.columns = df.columns.str.replace(" ", "") # remove spaces from column names
     df.rename(columns={"NumberofPlays": "Total Number of Plays for Date"}, inplace=True)
     dest_path = f"./downloads/processed_{os.path.basename(file_path)}"
-    df.to_csv(dest_path)
+    df.to_csv(dest_path, chunksize=10000)
     return dest_path
-
-# Time complexity - 0(n)2
-
-# This module utilises the pandas library a data analysis manipulation tool.
-# The data in read in chunks of 10,000 rows. (Note more details needed)
-# These chunks are the processed to get the Music number if plays for a Date. 
-# This is done by grouping the date and song, and compute the sum of group values, and the resulting dataframe saved to a list
-# Given the challenge of working with chunks, 
-# there is a compute the sum across the chunks after combining their result of their summation.
-# The resulting dataframe columns are renamed, to match the data representes and then written to a csv file for output.
